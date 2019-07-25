d1c5527036687289bbb329dbe0dbea67bfe89ff4
bleaktwig <bleak.twig@gmail.com>
2019-07-25 13:42:40 -0400

    upload all

--- init_setup/setup.py
+++ init_setup/setup.py
@@ -1,7 +1,9 @@
+import math
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib
 import matplotlib.pyplot as plt
+import ipywidgets as widgets
 
 np.set_printoptions(formatter={'float': '{: 0.5f}'.format}, suppress = True)
 matplotlib.rcParams.update({'font.size': 16})
