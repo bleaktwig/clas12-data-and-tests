3cdb9eb7d3505df506e1d10a4025c875509b9daf
bleaktwig <bleak.twig@gmail.com>
2019-07-26 10:15:15 -0400

    gee I sure love working 24/7

--- init_setup/setup.py
+++ init_setup/setup.py
@@ -1,3 +1,4 @@
+# ==- INITIAL SETUP -===============================================================================
 import math
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
@@ -7,3 +8,44 @@ import ipywidgets as widgets
 
 np.set_printoptions(formatter={'float': '{: 0.5f}'.format}, suppress = True)
 matplotlib.rcParams.update({'font.size': 16})
+
+# ==- PLOT PARAMETERS -=============================================================================
+loglogalpha = 0.70
+baralpha    = 0.85
+
+plot_size = (14,8)
+subplot_size = (7,4)
+
+# ==- COLOR PALETTE -===============================================================================
+c_red = "#e6194b" # red
+c_gre = "#3cb44b" # green
+c_yel = "#ffe119" # yellow
+c_blu = "#4363d8" # blue
+c_ora = "#f58231" # orange
+c_pur = "#911eb4" # purple
+c_cya = "#42d4f4" # cyan
+c_mag = "#f032e6" # magenta
+c_lim = "#bfef45" # lime
+c_pin = "#fabebe" # pink
+c_tea = "#469990" # teal
+c_lav = "#e6beff" # lavender
+c_bro = "#9A6324" # brown
+c_bei = "#fffac8" # beige
+c_mar = "#800000" # maroon
+c_min = "#aaffc3" # mint
+c_oli = "#808000" # olive
+c_apr = "#ffd8b1" # apricot
+c_nav = "#000075" # navy
+c_gra = "#a9a9a9" # gray
+c_whi = "#ffffff" # white
+c_bla = "#000000" # black
+
+accessibility_95    = [c_red, c_gre, c_blu, c_yel, c_ora, c_pur, c_cya,
+                       c_mag, c_lim, c_pin, c_tea, c_lav, c_bro, c_bei,
+                       c_mar, c_min, c_oli, c_apr, c_nav, c_gra, c_bla]
+accessibility_99    = [c_red, c_gre, c_blu, c_yel, c_ora, c_cya, c_mag,
+                       c_pin, c_tea, c_lav, c_bro, c_bei, c_mar, c_min,
+                       c_nav, c_gra, c_bla]
+accessibility_99_99 = [c_blu, c_yel, c_ora, c_pin, c_lav, c_mar, c_nav,
+                       c_gra, c_bla]
+accessibility_100   = [c_blu, c_yel, c_gra, c_bla]
